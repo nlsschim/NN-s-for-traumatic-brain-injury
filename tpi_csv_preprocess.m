7620aeeb95f976ea87be09e817985296b7804ae6
nlsschim <nlsschim@uw.edu>
2020-08-08 10:55:56 -0700

    features and targets put into matrix form

--- tpi_csv_preprocess.m
+++ tpi_csv_preprocess.m
@@ -1,13 +1,15 @@
 
 % Specify filepath for csv data
-filepath = '/Users/nlsschim/Documents/LAB/ML_TBI/June_data/DoD001_TPIHarmonicsTable.csv';
+filepath = '/Users/nlsschim/Documents/LAB/ML_TBI/john_data/DoD012_TPIHarmonicsTable.csv';
 feature_data = import_features(filepath); % extract features from csv
-target_data = import_target(filepath); % extract targets from csv
+%target_data = import_target(filepath); % extract targets from csv
 
 func = @(var) round(var,0); % set rounding fxn
 target_data = varfun(func, target_data); % round target data to be binary
 
-writetable(target_data, 'target_data.csv', 'Delimiter',',','QuoteStrings',true)
-writetable(feature_data, 'feature_data.csv', 'Delimiter', ',', 'QuoteStrings', true)
+writetable(target_data, 'target_data.csv', 'Delimiter',',','QuoteStrings',true);
+writetable(feature_data, 'feature_data.csv', 'Delimiter', ',', 'QuoteStrings', true);
 
-
+% convert tables to matrices
+targets = target_data{:,:}';
+features = feature_data{:,:}';
